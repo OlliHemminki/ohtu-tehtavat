8cfc1820257bc6f32a51b93afbb21ecf85af2752
OlliHemminki <olli.hemminki@gmail.com>
2020-02-29 18:54:50 +0200

    Viikon 4 tehtävä 4 tehty

--- viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ viikko4/Verkkokauppa/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -108,6 +108,74 @@ public class KauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
     }
+    
+    @Test
+    public void aloitaAsiointiMetodinKutsuminenNollaaEdellisenOstoksenTiedot() {
+    	when(viite.uusi()).thenReturn(42);
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(20);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "mehu", 8));
+        
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(5));
+        
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu oikeilla parametreilla
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), eq("33333-44455"), eq(8));
+    }
+    
+    @Test
+    public void jokainenMaksuSaaUudenViitenumeron() {
+    	when(viite.uusi()).
+    	thenReturn(42).
+    	thenReturn(31).
+    	thenReturn(19);
+    	
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(20);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "mehu", 8));
+        
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 2 eli mehua
+        k.tilimaksu("pekka", "12345");
+
+        // 
+        verify(viite, times(2)).uusi();
+    }
+    
+    @Test
+    public void tuotteenPoistaminenKoristaPoistaaTuotteen() {
+    	
+    	Tuote t = new Tuote(1, "maito", 5);
+    	
+    	when(viite.uusi()).thenReturn(42);
+    	
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(t);
+        
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        
+        k.lisaaKoriin(1);     // lisätään tuote 1 eli maito ostoskoriin       
+        k.poistaKorista(1);     // poistetaan tuote 1 eli maito ostoskorista
+        
+        verify(varasto, times(1)).palautaVarastoon(t);   
+    }
 }
 
 
